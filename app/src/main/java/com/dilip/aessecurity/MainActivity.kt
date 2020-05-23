75e8d6ec2a889dd789926e00a725cae5707c0324
Dilip Birajadar <dilip.birajadar@gamil.com>
2020-05-23 22:43:47 +0530

    Dilip Birajadar added code for the Java and kotline function call.

--- app/src/main/java/com/dilip/aessecurity/MainActivity.kt
+++ app/src/main/java/com/dilip/aessecurity/MainActivity.kt
@@ -2,11 +2,26 @@ package com.dilip.aessecurity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.dilip.aes_library.AES
+import com.dilip.aes_library.AESJava
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //String to encrypt using java
+        Log.e(""+":From Java Encrypted: ",AESJava.encrypt("Dilip"))
+
+        //String to encrypt in Kotlin
+        val aes : AES = AES()
+        val encrypt = aes.encrypt("Dilip")
+        Log.e(""+":From Kotlin Encrypted:",encrypt)
+
+        //decrypt in Kotlin
+        val dncrypt = aes.decrypt(encrypt)
+        Log.e(""+":From Kotlin Decrypted:",dncrypt)
     }
 }
